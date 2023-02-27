2fdafa70c5767ee4e288fd32d8a369d59b81aab5
Gabri-Ellie <gabrielli.vicente@madeiramadeira.com.br>
2023-02-27 16:58:19 -0300

    a

--- desafio.js
+++ desafio.js
@@ -1,31 +1,39 @@
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
-        function Enviar(){
-        var lado1= parseFloat(document.querySelector("#num1").value);
-        var lado2= parseFloat(document.querySelector("#num2").value);
-        var lado3= parseFloat(document.querySelector("#base").value);
-        if((lado1 >= lado2 + lado3) || (lado2 >= lado1 + lado3 )|| (lado3 >= lado1 + lado2)){
-            window.alert('não é um triângulo')
-        }
-    else if (lado1 == lado2&& lado2 == lado3){
-        window.alert('é um triângulo equilátero')
-    }else if ((lado1 == lado2 && lado1 != lado3)|| (lado2 == lado3 && lado2 != lado1)||( lado1 == lado3 && lado1 != lado2)){
-        window.alert('é um triângulo isósceles')
-    }else if( lado1 != lado2 && lado2 != lado3 ){
-        window.alert('é um triângulo escaleno')
-}else{
-    window.alert('Erro')
-} 
-S = (lado1+lado2+lado3)/2
-h = Math.sqrt(S*(S-lado1)*(S-lado2)*(S-base))* base/2
-d = Math.sqrt(Math.pow(h, 2) + Math.pow(lado1, 2))
-e = Math.sqrt(Math.pow(h, 2) + Math.pow(lado2, 2))
-
+function enviar(event) {
+    //event.preventDefault();
+    var a = parseInt(document.querySelector("#num1").value);
+    var b = parseInt(document.querySelector("#num2").value);
+    var c = parseInt(document.querySelector("#base").value);
+    if ((a >= b + c) || (b >= a + c) || (c >= a + b)) {
+        window.alert('não é um triângulo');
+    }
+    else if (a == b && b == c) {
+        window.alert('é um triângulo equilátero');
+    } else if ((a == b && a != c) || (b == c && b != a) || (a == c && a != b)) {
+        window.alert('é um triângulo isósceles');
+    } else if (a != b && b != c) {
+        window.alert('é um triângulo escaleno');
+    } else {
+        window.alert('Erro');
+    }
+    s = (a + b + c) / 2;
+    console.log(s);
+    h = Math.sqrt(s * (s - a) * (s - b) * (s - c)) / (c / 2);
+    console.log(h);
+    d = Math.sqrt(Math.pow(a, 2) - Math.pow(h, 2));
+    console.log(d);
+    e = Math.sqrt(Math.pow(b, 2) - Math.pow(h, 2));
+    console.log(e);
 
     const tri = document.querySelector('.arrow-up');
-    tri.style.borderLeftWidth = d;
-    tri.style.borderRightWidth = e;
-    tri.style.borderBottomWidth = h;
+    tri.style.borderLeft = `${parseInt(d)}cm solid transparent`;
+    tri.style.borderRight = `${parseInt(e)}cm solid transparent`;
+    tri.style.borderBottom = `${parseInt(h)}cm solid pink`;
 
 }
-})
+function limpar(event) {
+
+    l1= document.getElementById("num1").value='';
+    l2= document.getElementById("num2").value='';
+    l3= document.getElementById("base").value='';
+
+}
