c917177b33ef5dc4e320f77e3f5fcb9d8ba15294
Gabri-Ellie <gabrielli.vicente@madeiramadeira.com.br>
2023-03-07 16:56:46 -0300

    aa

--- desafio.js
+++ desafio.js
@@ -1,37 +1,54 @@
+var inputA = document.getElementById("num1");
+var inputB = document.getElementById("num2");
+var inputC = document.getElementById("base");
+var t1 = document.getElementById("texto1");
+var t2 = document.getElementById("texto2")
+var Base = document.getElementById("quadl1");
+var Altura = document.getElementById("quadl2");
 function isEmpty(str) {
 
     return (!str || str.length === 0);
 
 }
+function hidequad() {
+    document.getElementById("tra").style.display = "flex";
+    document.getElementById("quad").style.display = "none";
+    document.getElementById("cat").style.display = "none";
+}
+function hidetrig() {
+    document.getElementById("tra").style.display = "none";
+    document.getElementById("quad").style.display = "flex";
+    document.getElementById("cat").style.display = "none";
+}
 
-function enviar() {
 
-    var a = parseInt(document.getElementById("num1").value);
-    var b = parseInt(document.getElementById("num2").value);
-    var c = parseInt(document.getElementById("base").value);
-    var t = document.getElementById("texto");
+function trig() {
+
+    var a = parseInt(inputA.value);
+    var b = parseInt(inputB.value);
+    var c = parseInt(inputC.value);
 
     if (
         isEmpty(a) ||
         isEmpty(b) ||
         isEmpty(c)
     ) {
-        var te = document.createTextNode("insira valores válidos");
+        t1.textContent = "insira valores válidos";
+
         return;
     }
     if ((a >= b + c) || (b >= a + c) || (c >= a + b)) {
-        var te = document.createTextNode('não é um triângulo');
+        t1.textContent = 'não é um triângulo';
     } else if (a != b && b != c && a != c) {
-        var te = document.createTextNode('é um triângulo escaleno');
+        t1.textContent = 'é um triângulo escaleno';
     } else if (a == b && b == c && a == c) {
-        var te = document.createTextNode('é um triângulo equilátero');
+        t1.textContent = 'é um triângulo equilátero';
     } else if ((a == b && a != c) || (b == c && b != a) || (a == c && a != b)) {
-        var te = document.createTextNode('é um triângulo isósceles');
+        t1.textContent = 'é um triângulo isósceles';
     } else {
-        var te = document.createTextNode('Erro');
+        t1.textContent = 'Erro';
     }
 
-    t.appendChild(te);
 
     s = (a + b + c) / 2;
     console.log(s);
@@ -48,13 +65,41 @@ function enviar() {
     tri.style.borderBottom = `${parseFloat(h)}cm solid darkblue`;
 
 }
+function quad() {
+    var ba = parseFloat(Base.value);
+    var al = parseFloat(Altura.value);
+
+    if (
+        isEmpty(ba) ||
+        isEmpty(al)
+    ) {
+        t2.textContent = "Insira valores válidos";
+
+        return;
+    } else if (ba == al) {
+        t2.textContent = "É um quadrado";
+    } else {
+        t2.textContent = "É um retângulo";
+    }
+    const qua = document.querySelector('.square');
+    qua.style.width = `${ba}cm `;
+    qua.style.height = `${al}cm`;
+    qua.style.backgroundColor = "#7a297a";
+}
 function limpar() {
 
-    document.getElementById("num1").value = '';
-    document.getElementById("num2").value = '';
-    document.getElementById("base").value = '';
+    document.getElementById("texto1").textContent = "";
+    document.getElementById("texto2").textContent = "";
 
-    const tri = document.querySelector('.arrow-up');
-    tri.style.border = 'none';
+    inputA.value = '';
+    inputB.value = '';
+    inputC.value = '';
+    Base.value = '';
+    Altura.value = '';
+
+
+    document.querySelector('.arrow-up').style = "none";
+    document.querySelector('.square').style = "none";
+
 
 }
